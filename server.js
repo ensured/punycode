95a03d095458abccef839667d4d75afe61c46a75
ensured <walnuts@gmail.com>
2023-03-14 15:25:33 -0700

    added if the handle if available or not

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 'use strict';
 const express = require('express');
+const axios = require('axios');
 const punycode = require('punycode/');
 const app = express();
-
 // Constants
 const PORT = process.env.PORT || 8080;
-
 // middleware to parse req.body.<name>
 app.use(express.urlencoded({ extended: true }));
-
 // serve static files
 app.use(express.static('public'));
+
+async function checkIsHandleAvail(handle) {
+  const response = await axios.get('https://bff.handle.me/lookupAddress', {
+      headers: {
+        'authority': 'bff.handle.me',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'origin': 'https://handle.me',
+        'referer': 'https://handle.me/',
+        'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+        'x-handle': handle,
+      }
+  });
+  let res_json = await response.data;
+  // check if res_json.address exists if not then the handle is available
+  if (res_json.address) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 
 function decimalToHexadecimal(decimal) {
   const hex = decimal.toString(16);
@@ -50,15 +77,18 @@ function getCodePointsAndCountOfCodepoints(str) {
 // route to handle Punycode to Unicode conversion
 app.post('/punycode_to_unicode', (req, res) => {
   let punycodeStr = req.body.punycode;
+  // checkIsHandleAvail(req.body.unicode);
   if (punycodeStr.startsWith('xn--')) {
     punycodeStr = punycodeStr.substring(4);
   }
   try {
     const decodedStr = punycode.decode(punycodeStr);
-    res.send({
-      'decodedStr': decodedStr,
+    const handleAvail = checkIsHandleAvail(`xn--${punycodeStr}`).then(x => { res.send({
+      'decodedStr': `${decodedStr} avail: <a href="https://handle.me/xn--${punycodeStr}">${x}</a><br>`,
       "charLength": getCodePointsAndCountOfCodepoints(decodedStr)
     });
+  });
+ 
   } catch (e) {
     res.send('Invalid Punycode');
   }
@@ -70,10 +100,12 @@ app.post('/unicode_to_punycode', (req, res) => {
   const count = getCodePointsAndCountOfCodepoints(unicodeText);
   try {
     const punycodeStr = punycode.encode(unicodeText);
-    res.send({
-      'punycodeStr': `xn--${punycodeStr}`,
+    const handleAvail = checkIsHandleAvail(`xn--${punycodeStr}`).then(x => { res.send({
+      'punycodeStr': `xn--${punycodeStr} avail: <a href="https://handle.me/xn--${punycodeStr}">${x}</a>`,
       "charLength": count
+    });
   });
+    
   } catch (e) {
     res.send('Invalid Unicode');
   }
