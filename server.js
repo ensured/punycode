03c2c67896af4b397bb23633d1d62b26a4c4b3cb
ensured <walnuts@gmail.com>
2023-03-13 18:01:17 -0700

    fixed a lot of things and made code easier to read.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
 const express = require('express');
-const punycode = require('punycode');
+const punycode = require('punycode/');
 const app = express();
 
 // Constants
 const PORT = process.env.PORT || 8080;
 
-// middleware to parse request body
+// middleware to parse req.body.name
 app.use(express.urlencoded({ extended: true }));
 
 // serve static files
@@ -14,21 +14,23 @@ app.use(express.static('public'));
 
 // route to handle Punycode to Unicode conversion
 app.post('/punycode_to_unicode', (req, res) => {
-  const punycodeStr = req.body.punycode;
+  let punycodeStr = req.body.punycode;
+  if (punycodeStr.startsWith('xn--')) {
+    punycodeStr = punycodeStr.substring(4);
+  }
   try {
-    const decodedStr = punycode.toUnicode(punycodeStr);
+    const decodedStr = punycode.decode(punycodeStr);
     res.send(decodedStr + `[${decodedStr.length}]`);
   } catch (e) {
     res.send('Invalid Punycode');
   }
 });
 
-// route to handle Unicode to Punycode conversion
 app.post('/unicode_to_punycode', (req, res) => {
-  const unicodeStr = req.body.unicode;
+  const unicodeText = req.body.unicode;
   try {
-    const encodedStr = punycode.toASCII(unicodeStr);
-    res.send(encodedStr);
+    const punycodeStr = punycode.encode(unicodeText);
+    res.send(`xn--${punycodeStr}`);
   } catch (e) {
     res.send('Invalid Unicode');
   }
